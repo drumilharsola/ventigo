a0544a0f913b074f60aac2a7312cae498443b859
dhrumil.harsola <dhrumil.harsola@siemens.com>
2026-03-13 14:20:44 +0530

    Fix blank page: add auth hydration, custom app icon, rename app label to Unburden

--- unburden_app/lib/main.dart
+++ unburden_app/lib/main.dart
@@ -4,17 +4,30 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'config/brand.dart';
 import 'config/routes.dart';
 import 'config/theme.dart';
+import 'state/auth_provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: UnburdenApp()));
 }
 
-class UnburdenApp extends ConsumerWidget {
+class UnburdenApp extends ConsumerStatefulWidget {
   const UnburdenApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<UnburdenApp> createState() => _UnburdenAppState();
+}
+
+class _UnburdenAppState extends ConsumerState<UnburdenApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Hydrate auth state from secure storage so the router can proceed.
+    Future.microtask(() => ref.read(authProvider.notifier).hydrate());
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final router = ref.watch(routerProvider);
 
     return MaterialApp.router(
